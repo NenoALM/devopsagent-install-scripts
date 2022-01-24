34879df0a8f70d7618ba75f3ed58adf02b4bf290
Neno Loje <NenoLoje@users.noreply.github.com>
2022-01-24 16:29:36 +0100

    Add files via upload

---
+++ Install-DevOpsAgent-Service.ps1
@@ -0,0 +1,59 @@
+#-------------------------------------------------------------------------------------------------#
+# File        : Install-DevOpsAgent-Service.ps1
+# Description : Installs a new Azure DevOps Agent and configures it as a Windows service.
+#-------------------------------------------------------------------------------------------------#
+
+param (
+  # URL to Azure DevOps organization (e.g. https://dev.azure.com/<org>)
+  [Parameter(Mandatory=$true)]
+  [string]$azureDevOpsURL,
+
+  # Personal access token with scope of 'Manage Agent Pools'
+  [Parameter(Mandatory=$true)]
+  [SecureString]$azureDevOpsPAT,
+
+  # Name of agent pool to register the agent against
+  [Parameter(Mandatory=$true)]
+  [string]$agentPool,
+
+  # Desired agent name (typically same as machine name)
+  [Parameter(Mandatory=$true)]
+  [string]$agentName,
+
+  # Service user for Windows service
+  [Parameter(Mandatory=$true)]
+  [string]$agentServiceUser,
+  
+  # Service user password for Windows service
+  [Parameter(Mandatory=$true)]
+  [SecureString]$agentServicePassword,
+
+  # Agent Directory
+  [Parameter(Mandatory=$true)]
+  [string]$agentDirectory,
+
+  # Agent ZIP
+  [Parameter(Mandatory=$true)]
+  [string]$agentZip
+)
+
+# Note: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.  
+$ErrorActionPreference = "Stop"
+
+Write-Host "Exctracting DevOps Agent..."
+Expand-Archive -Path $agentZip -Destination $agentDirectory -Force
+
+Write-Host "Configuring DevOps Agent..."
+$patCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "pat", $azureDevOpsPAT
+$token = $patCredential.GetNetworkCredential().password
+write-host "Length of token: "$token.Length" (should equal 52)"
+
+$pwdCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "service", $agentServicePassword
+$svcUserPwd = $pwdCredential.GetNetworkCredential().password
+write-host "Length of pwd: "$svcUserPwd.Length
+
+$config = "$agentDirectory/config.cmd"
+iex "$config --version"
+iex "$config --unattended --url $azureDevOpsURL --auth pat --token $token --pool $agentPool --agent $agentName --runAsService --windowsLogonAccount $agentServiceUser --windowsLogonPassword $svcUserPwd"
+
+Write-Host "Done."
---
+++ Install-DevOpsAgents.ps1
@@ -0,0 +1,76 @@
+#-------------------------------------------------------------------------------------------------#
+# File        : Install-DevOpsAgent.ps1
+# Description : Installs a new Azure DevOps Agent and configures it as a Windows service.
+#-------------------------------------------------------------------------------------------------#
+
+#[CmdletBinding()]
+param (
+  # URL to Azure DevOps organization (e.g. https://dev.azure.com/<org>)
+  [Parameter(Mandatory=$true)]
+  [string]$azureDevOpsURL,
+
+  # Personal access token with scope of 'Manage Agent Pools'
+  [Parameter(Mandatory=$true)]
+  [SecureString]$azureDevOpsPAT,
+
+  # Name of agent pool to register the agent against
+  [Parameter(Mandatory=$true)]
+  [string]$agentPool,
+
+  # Desired agent name (typically same as machine name)
+  [Parameter(Mandatory=$true)]
+  [string]$agentName,
+
+  # Service user for Windows service
+  [Parameter(Mandatory=$true)]
+  [string]$agentServiceUser,
+  
+  # Service user password for Windows service
+  [Parameter(Mandatory=$true)]
+  [SecureString]$agentServicePassword,
+
+  [Parameter(Mandatory=$true)]
+  [string]$agentZip = "C:/agent.zip",
+
+  [ValidatePattern("[a-zA-Z]")]
+  [ValidateLength(1, 1)]
+  [string] $driveLetter = 'C'
+)
+
+# Note: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.  
+$ErrorActionPreference = "Stop"
+
+Write-Output "+++ BEGIN: Install Deploy-Agent as Service +++"
+
+$deployDirectory = Join-Path -Path ($driveLetter + ":") -ChildPath "Deploy-$AgentName"
+
+Write-Output 'Hello'
+
+./Install-DevOpsAgent-Service.ps1 `
+    -azureDevOpsURL $azureDevOpsURL `
+    -azureDevOpsPAT $azureDevOpsPAT `
+    -agentPool "$agentPool-deploy" `
+    -agentName "Deploy-$agentName" `
+    -agentServiceUser $agentServiceUser `
+    -agentServicePassword $agentServicePassword `
+    -agentDirectory $deployDirectory `
+    -agentZip $agentZip
+
+Write-Output "+++ END: Install Deploy-Agent as Service +++"
+
+Write-Output "+++ BEGIN: Install Agent in interactive mode +++"
+
+$agentDirectory = Join-Path -Path ($driveLetter + ":") -ChildPath "Agent-$AgentName"
+
+./Install-DevOpsAgent-Interactive.ps1 `
+    -azureDevOpsURL $azureDevOpsURL `
+    -azureDevOpsPAT $azureDevOpsPAT `
+    -agentPool "$agentPool-agent" `
+    -agentName "Deploy-$agentName" `
+    -agentServiceUser $agentServiceUser `
+    -agentServicePassword $agentServicePassword `
+    -agentDirectory $agentDirectory `
+    -agentZip $agentZip
+
+Write-Output "+++ END: Install Agent in interactive mode +++"
+Write-Output "All Done."
